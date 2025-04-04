129acd38f316ad4b0051948431e6ecbcfb6574c9
jason-devprog <jasonpiit9@gmail.com>
2025-04-04 11:31:43 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,22 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
-import electron from 'vite-plugin-electron'
+// import react from '@vitejs/plugin-react'
+// import electron from 'vite-plugin-electron'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    electron({
-      entry: 'electron/main/index.js'
-    })
-  ],
-  base: './',
-  build: {
-    outDir: 'dist/renderer',
-    assetsDir: '.',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/renderer/index.html')
-      }
-    },
-    emptyOutDir: true,
-    sourcemap: true,
-    cssCodeSplit: false
+  main: {
+    build: {
+      outDir: 'dist/main'
+    }
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src/renderer/src')
+  preload: {
+    build: {
+      outDir: 'dist/preload'
+    }
+  },
+  renderer: {
+    build: {
+      outDir: 'dist/renderer'
     }
   }
 })
